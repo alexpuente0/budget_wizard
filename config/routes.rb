fec235c3d3cdc2bf1a944ac17344c514b9df7df5
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-09-10 01:19:48 -0500

    created joint table and modified schema

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
   devise_scope  :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
+
+   resources :groups, only: [:index, :show, :create, :new] do
+    resources :expenses, only: [:new, :create, :show]
+  end
   
-  get '/expenses/index/:group_id', to: "expenses#index", as: 'expense_index'
-  get '/expenses/show/:expense_id/:group_id', to: "expenses#show", as: 'expense_detail'
-  get '/expenses/new/:group_id', to: "expenses#new", as: 'expense_new'
-  post '/expenses/new', to: "expenses#create", as: 'expense_create'
-  resources :groups
+  # get '/expenses/index/:group_id', to: "expenses#index", as: 'expense_index'
+  # get '/expenses/show/:expense_id/:group_id', to: "expenses#show", as: 'expense_detail'
+  # get '/expenses/new/:group_id', to: "expenses#new", as: 'expense_new'
+  # post '/expenses/new', to: "expenses#create", as: 'expense_create'
+  # resources :groups
 
   get '/', to: "splash#index", as: 'splash'
 
